babee565b0a421055f99c1df0ffe3c17317596de
Keren Gantman <keren.gantman@post.runi.ac.il>
2025-11-12 23:08:20 +0200

    finishCheers

--- Cheers.java
+++ Cheers.java
@@ -4,10 +4,27 @@
 Great work boaz!
 */
 
-
 // Prints a crowd cheering output.
 public class Cheers {
-        public static void main(String[] args) {
-	    // Replace this comment with your code
+    public static void main(String[] args) {
+        String RUNI = args[0];
+        String number = args[1];
+        int num = Integer.parseInt(number);
+        String vowels = "AEFHILMNORSX";
+        int i = 0;
+        while (i < RUNI.length()) {
+            char c = RUNI.charAt(i);
+            if (vowels.indexOf(c) == -1) {
+                
+                System.out.println("GIVE me a " + c +": " + c + "!");
+            } else {
+                System.out.println("GIVE me an " + c + ":" + c + "!");
+            }
+            i++;
         }
+        System.out.println("What does that spell?");
+        for (int j = 0; j < num; j++) {
+            System.out.println(RUNI + "!!!");
+        }
+    }
 }
