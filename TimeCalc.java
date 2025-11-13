ce0be18cc31d809b38cae155f785e4cb56234171
Keren Gantman <keren.gantman@post.runi.ac.il>
2025-11-13 22:08:16 +0200

    finish

--- TimeCalc.java
+++ TimeCalc.java
@@ -1,22 +1,22 @@
 public class TimeCalc {
     public static void main(String[] args) {
-        String clock= args[0];
-        String minutesToAdd= args[1];
-        int hours= Integer.parseInt(clock.substring(0,2));
-        int minutes= Integer.parseInt(clock.substring(3,5));    
-        int minutesAdd= Integer.parseInt(minutesToAdd);     
-        int totalminutes= hours*60 + minutes + minutesAdd;  
-        int totalhours= (totalminutes/60)%24;
-        int newminutes= totalminutes-minutesAdd;
-        if (totalhours<10){
-            System.out.print("0"+totalhours+":"); 
+        String clock = args[0];
+        String minutesToAdd = args[1];
+        int hours = Integer.parseInt(clock.substring(0, 2));
+        int minutes = Integer.parseInt(clock.substring(3, 5));
+        int minutesAdd = Integer.parseInt(minutesToAdd);
+        int totalminutes = hours * 60 + minutes + minutesAdd;
+        int totalhours = (totalminutes / 60) % 24;
+        int newminutes = totalminutes % 60;
+        if (totalhours < 10) {
+            System.out.print("0" + totalhours + ":");
         } else {
-            System.out.print(totalhours+":");
+            System.out.print(totalhours + ":");
         }
-        if (newminutes%60<10){
-            System.out.println("0"+newminutes%60);
+        if (newminutes % 60 < 10) {
+            System.out.println("0" + newminutes % 60);
         } else {
-            System.out.println(newminutes%60);
+            System.out.println(newminutes % 60);
         }
     }
 }
