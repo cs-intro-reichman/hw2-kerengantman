3ef99e4d24fff30b4c2543ec6e4960e84038ad8f
Keren Gantman <keren.gantman@post.runi.ac.il>
2025-11-13 22:36:39 +0200

    finish3

--- Collatz.java
+++ Collatz.java
@@ -3,28 +3,27 @@ public class Collatz {
 	public static void main(String args[]) {
 		Integer n = Integer.parseInt(args[0]);
 		String mode = args[1];
-		int counter = 1;
+		int counter = 0;
 		int j = 0;
-		boolean first = true;
 		if (mode.equals("c") || mode.equals("concise")) {
 			System.out.println("Every one of the first " + n + " hailstone sequences reached 1.");
 		} else {
 			for (int i = 1; i <= n; i++) {
 				j = i;
 				System.out.print(j + " ");
-				while ((j != 1) || (first == true)) {
-					first = false;
+				// count the numbers printed in this sequence (start with the initial number)
+				counter = 1;
+				// perform at least one Collatz step and continue until we reach 1
+				do {
 					if (j % 2 == 0) {
 						j = j / 2;
-						System.out.print(j + " ");
 					} else {
 						j = (3 * j) + 1;
-						System.out.print(j + " ");
 					}
+					System.out.print(j + " ");
 					counter++;
-				}
+				} while (j != 1);
 				System.out.println("(" + counter + ")");
-				counter = 0;
 			}
 			System.out.println("Every one of the first " + n + " hailstone sequences reached 1.");
 		}
