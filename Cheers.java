95a0c8ae71002bc6e5818081398746e7b42d285a
Keren Gantman <keren.gantman@post.runi.ac.il>
2025-11-13 22:30:32 +0200

    finish2

--- Cheers.java
+++ Cheers.java
@@ -8,23 +8,25 @@ Great work boaz!
 public class Cheers {
     public static void main(String[] args) {
         String RUNI = args[0];
-        String number = args[1];
-        int num = Integer.parseInt(number);
-        String vowels = "AEFHILMNORSXaefhlmnorsx";
-        int i = 0;
-        while (i < RUNI.length()) {
+        int num = Integer.parseInt(args[1]);
+
+        String anLetters = "AEFHILMNORSX";
+
+        for (int i = 0; i < RUNI.length(); i++) {
             char c = RUNI.charAt(i);
-            if (vowels.indexOf(c) == -1) {
-                
-                System.out.println("GIVE me a " + c +": " + c + "!");
-            } else {
-                System.out.println("GIVE me an " + c + ":" + c + "!");
-            }
-            i++;
+            char upper = Character.toUpperCase(c);
+
+            boolean useAn = anLetters.indexOf(upper) >= 0;
+            String article = useAn ? "an" : "a";
+            String spacing = useAn ? " " : "  ";
+
+            System.out.println("Give me " + article + spacing + upper + ": " + upper + "!");
         }
+
         System.out.println("What does that spell?");
+        String shouted = RUNI.toUpperCase();
         for (int j = 0; j < num; j++) {
-            System.out.println(RUNI + "!!!");
+            System.out.println(shouted + "!!!");
         }
     }
 }
