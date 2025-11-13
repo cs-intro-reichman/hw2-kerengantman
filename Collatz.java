75d2c4012e2b9363982dfcdd417a1008022159e4
Keren Gantman <keren.gantman@post.runi.ac.il>
2025-11-13 23:35:53 +0200

    finis8

--- Collatz.java
+++ Collatz.java
@@ -1,31 +1,40 @@
 
 public class Collatz {
 	public static void main(String args[]) {
-		Integer n = Integer.parseInt(args[0]);
+		if (args.length < 2) {
+			System.err.println("Usage: java Collatz <n> <mode>");
+			return;
+		}
+
+		int n = Integer.parseInt(args[0]);
 		String mode = args[1];
-		int counter = 0;
-		int j = 0;
+
 		if (mode.equals("c") || mode.equals("concise")) {
 			System.out.println("Every one of the first " + n + " hailstone sequences reached 1.");
-		} else {
-			for (int i = 1; i <= n; i++) {
-				j = i;
-				System.out.print(j + " ");
-				// count the numbers printed in this sequence (start with the initial number)
-				counter = 1;
-				// perform at least one Collatz step and continue until we reach 1
-				do {
-					if (j % 2 == 0) {
-						j = j / 2;
-					} else {
-						j = (3 * j) + 1;
-					}
-					System.out.print(j + " ");
-					counter++;
-				} while (j != 1);
-				System.out.println("(" + counter + ")");
-			}
-			System.out.println("Every one of the first " + n + " hailstone sequences reached 1.");
+			return;
 		}
+
+		for (int i = 1; i <= n; i++) {
+			int j = i;
+			System.out.print(j + " ");
+
+			// count printed numbers in this sequence (start with the initial number)
+			int counter = 1;
+
+			// perform Collatz steps until we reach 1
+			while (j != 1) {
+				if (j % 2 == 0) {
+					j = j / 2;
+				} else {
+					j = 3 * j + 1;
+				}
+				System.out.print(j + " ");
+				counter++;
+			}
+
+			System.out.println("(" + counter + ")");
+		}
+
+		System.out.println("Every one of the first " + n + " hailstone sequences reached 1.");
 	}
 }
